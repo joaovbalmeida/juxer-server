e0cf6cb308f2a61b755efcf931e688b775309828
joaovbalmeida <joaovbalmeida@gmail.com>
2018-08-02 23:15:26 -0300

    1.0

--- src/services/events/events.service.js
+++ src/services/events/events.service.js
@@ -13,11 +13,18 @@ module.exports = function (app) {
     paginate
   };
 
+  const createChannel = (event, context) => {
+    app.channel(`rooms/${event._id}`).join(context.params.connection);
+  };
+
   // Initialize our service with any options it requires
   app.use('/events', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('events');
+
+  service.on('created', createChannel);
+  service.publish('created', data => app.channel(`rooms/${data._id}`).send(data));
 
   service.hooks(hooks);
 };
