32b0b6da19583d53fe44e721d8c0f8d19707d236
joaovbalmeida <joaovbalmeida@gmail.com>
2018-09-27 22:25:45 -0300

    fixed port number

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
 // const port2 = app.get('port2');
-const server = app.listen(process.env.PORT2 || port);
+const server = app.listen(process.env.PORT || port);
 
 // Spotify Oauth
 // const sptApp = require('./spotify-server');
