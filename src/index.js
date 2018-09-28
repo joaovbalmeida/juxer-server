0ac6f35b5375b3e63ae2987667be2b0fc76215b1
joaovbalmeida <joaovbalmeida@gmail.com>
2018-09-27 22:20:57 -0300

    disabled spt for heroku

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-const port2 = app.get('port2');
+// const port2 = app.get('port2');
 const server = app.listen(process.env.PORT2 || port);
 
 // Spotify Oauth
-const sptApp = require('./spotify-server');
-sptApp.listen(process.env.PORT || port2, () => logger.info('Spotify Oauth app listening on port 3000!'));
+// const sptApp = require('./spotify-server');
+// sptApp.listen(process.env.PORT || port2, () => logger.info('Spotify Oauth app listening on port 3000!'));
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
